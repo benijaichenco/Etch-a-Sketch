fe5184d5ceaaf80fcf91a8d3ddd72aae19db3a73
faderb <benjaminjaichenco@gmail.com>
2022-11-23 20:44:56 +0200

    create content and container constants

---
+++ script.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('.container');
+//create content div for the grid
+const content = document.createElement('div');
+container.appendChild(content).classList.add('content');

11535ddb2472c4f169989c51e3ba1ea3aa40e534
faderb <benjaminjaichenco@gmail.com>
2022-11-24 14:29:38 +0200

    generate 'createGrid' function

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const container = document.querySelector('.container');
 //create content div for the grid
 const content = document.createElement('div');
 container.appendChild(content).classList.add('content');
+
+//create a default grid 16x16
+function createGrid() {
+    content.style.gridTemplateColumns = 'repeat(16, 1fr)';
+    //for loop
+    for (let i = 0; i < (16 * 16); i++) {
+        const box = document.createElement('div');
+        content.appendChild(box).classList.add('pixel');
+        box.addEventListener('mouseover', () => {
+            box.style.background = 'black';
+        });
+    }
+}
+createGrid();
